e5a2e86e19af3a39c231795ce0a94471a4c4ccb4
beata <beata@beata.local>
2018-09-29 12:53:22 +0200

    Zadanie 1: show/hide menu

---
+++ js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+
+document.addEventListener ('DOMContentLoaded', function (){
+    console.log('Beata');
+
+    var menuButton = document.querySelector('.for-dropdown');
+    console.log(menuButton);
+    var subMenu = document.querySelector('.dropdown');
+    
+    menuButton.addEventListener('mouseover', function() {    
+        subMenu.style.display = 'block';    
+    });
+    
+    menuButton.addEventListener('mouseout', function() {    
+        subMenu.style.display = 'none';    
+    });
+    
+    
+      
+});
+
+
+
