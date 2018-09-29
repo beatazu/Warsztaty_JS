bd071d6586b1c98e1eeff34756c67f3c714273d4
beata <beata@beata.local>
2018-09-29 14:50:18 +0200

    Zadanie 2: more/less button

--- js/app.js
+++ js/app.js
@@ -17,6 +17,33 @@ document.addEventListener ('DOMContentLoaded', function (){
         subMenu.style.display = 'none';    
     });
     
+    var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons);
+    
+    console.log(buttons[0].previousElementSibling);
+    
+    
+    function showHide () {
+        var textArea = this.previousElementSibling;   
+        
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+                
+        } else {
+            textArea.style.display = 'none';
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';   
+        }  
+    }
+    
+    
+    
+    //buttons[0].addEventListener('click', showHide);
+         
+    
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
     
       
 });
